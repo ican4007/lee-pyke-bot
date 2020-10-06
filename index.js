d6c8053e8912b83d601701316b529bd280a3b857
ican4007 <ican4006@gmail.com>
2020-10-07 00:39:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.on('message', (message) => {
   if(message.author.bot) return;
 
   if(message.content == 'ping') {
-    return message.reply('pong');
+    return message('pong');
   }
 
   if(message.content == '파잌봇 정보') {
